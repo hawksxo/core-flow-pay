68c2e4a18dbc8008fe96d8a1ab92eb570c89a643
hawksxo <losadabolivar@gmail.com>
2026-09-03 14:36:39 -0500

    feat: :tada: oh shit, here we go again

---
+++ src/main/java/com/hawksxo/core_flow_pay/CoreFlowPayApplication.java
@@ -0,0 +1,13 @@
+package com.hawksxo.core_flow_pay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreFlowPayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreFlowPayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hawksxo/core_flow_pay/CoreFlowPayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hawksxo.core_flow_pay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreFlowPayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
