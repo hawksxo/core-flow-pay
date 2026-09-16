f5a98f5ed3f6af8fae7f2aca6e648d59f63c8e95
hawksxo <losadabolivar@gmail.com>
2026-09-16 08:15:14 -0500

    feat: :sparkles: se habilitaron 2 nuevos endpoints en el módulo
    - Se creó el método de 'activateSubscription' para modificar el estado de una subscripción pendiente.
    - Se implementó el método de 'cancelSubscription' para alterar su estado sin importar cuál fuera.

--- src/main/java/com/hawksxo/core_flow_pay/infrastructure/entrypoints/rest/SubscriptionController.java
+++ src/main/java/com/hawksxo/core_flow_pay/infrastructure/entrypoints/rest/SubscriptionController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.hawksxo.core_flow_pay.application.dtos.CreateSubscriptionCommand;
+import com.hawksxo.core_flow_pay.application.usecases.ActivateSubscriptionUseCase;
+import com.hawksxo.core_flow_pay.application.usecases.CancelSubscriptionUseCase;
 import com.hawksxo.core_flow_pay.application.usecases.CreateSubscriptionUseCase;
 import com.hawksxo.core_flow_pay.domain.models.Subscription;
 
@@ -12,15 +14,23 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
-
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @RestController
 @RequestMapping("/api/v1/subscriptions")
 public class SubscriptionController {
     private final CreateSubscriptionUseCase createSubscriptionUseCase;
+    private final ActivateSubscriptionUseCase activateSubscriptionUseCase;
+    private final CancelSubscriptionUseCase cancelSubscriptionUseCase;
 
-    public SubscriptionController(CreateSubscriptionUseCase createSubscriptionUseCase) {
+    public SubscriptionController(
+            CreateSubscriptionUseCase createSubscriptionUseCase,
+            ActivateSubscriptionUseCase activateSubscriptionUseCase,
+            CancelSubscriptionUseCase cancelSubscriptionUseCase) {
         this.createSubscriptionUseCase = createSubscriptionUseCase;
+        this.activateSubscriptionUseCase = activateSubscriptionUseCase;
+        this.cancelSubscriptionUseCase = cancelSubscriptionUseCase;
     }
 
     public record CreateSubscriptionRequest(String userId, String planId) {}
@@ -30,7 +40,7 @@ public class SubscriptionController {
             @RequestBody CreateSubscriptionRequest request,
             @RequestHeader("X-Idempotency-Key") String idempotencyKey) {
 
-                CreateSubscriptionCommand command = new CreateSubscriptionCommand(
+        CreateSubscriptionCommand command = new CreateSubscriptionCommand(
                 request.userId(),
                 request.planId(),
                 idempotencyKey
@@ -40,5 +50,16 @@ public class SubscriptionController {
 
         return ResponseEntity.status(HttpStatus.CREATED).body(subscription);
     }
-    
+
+    @PatchMapping("/{id}/activate")
+    public ResponseEntity<Void> activateSubscription(@PathVariable("id") String id) {
+        activateSubscriptionUseCase.execute(id);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PatchMapping("/{id}/cancel")
+    public ResponseEntity<Void> cancelSubscription(@PathVariable("id") String id) {
+        cancelSubscriptionUseCase.execute(id);
+        return ResponseEntity.noContent().build();
+    }
 }
